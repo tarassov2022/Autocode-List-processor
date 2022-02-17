8fa1664aab3d4c8c62d9e23408d0c33c38b51f00
Andrei Tarassov <tamogenia@gmail.com>
2022-02-17 10:33:45 +0300

    Внес дополнительные проверки

--- src/main/java/com/epam/tat/listprocessor/impl/ListProcessor.java
+++ src/main/java/com/epam/tat/listprocessor/impl/ListProcessor.java
@@ -92,9 +92,26 @@ public class ListProcessor implements IListProcessor {
 	public List<String> getSortedListByCountOfVowels(List<String> list) {
 		if (list==null) throw new ListProcessorException("The list is null");
 		if (list.isEmpty()||list.contains("")) throw new ListProcessorException("The list is empty or contains empty elements");
+		for (String string:list){
+			char stringElement;
+			boolean anyVowels=false;
+			for(int j=0;j<string.length();j++)
+			{
+				stringElement=string.charAt(j);
+				if(stringElement=='a'||stringElement=='e'||stringElement=='i'||stringElement=='o'||stringElement=='u'||stringElement=='A'||stringElement=='E'||stringElement=='I'||stringElement=='O'||stringElement=='U')
+				{
+					anyVowels=true;
+				}
+			}
+			if(anyVowels==false)
+				throw new ListProcessorException("There are no vowels!");
+
+
+		}
 		Collections.sort(list);
 		Collections.sort(list,(o1, o2) -> o1.replaceAll("[^aeiouAEIOU]","").length()-o2.replaceAll("[^aeiouAEIOU]","").length());
 		return list;
+
 
 	}
 
@@ -115,6 +132,30 @@ public class ListProcessor implements IListProcessor {
 	public List<String> getSortedListByCountOfConsonants(List<String> list) {
 		if (list==null) throw new ListProcessorException("The list is null");
 		if (list.isEmpty()||list.contains("")) throw new ListProcessorException("The list is empty or contains empty elements");
+		for (String string:list){
+			char stringElement;
+			boolean anyConsonants=false;
+			for(int j=0;j<string.length();j++)
+			{
+				stringElement=string.charAt(j);
+				if(stringElement=='b'||stringElement=='B'||stringElement=='c'||stringElement=='C'||stringElement=='d'||
+						stringElement=='D'||stringElement=='f'||stringElement=='F'||stringElement=='g'||stringElement=='G'||
+						stringElement=='j'||stringElement=='J'||stringElement=='k'||stringElement=='K'||stringElement=='l'||
+						stringElement=='L'||stringElement=='m'||stringElement=='M'||stringElement=='n'||stringElement=='N'||
+						stringElement=='p'||stringElement=='P'||stringElement=='q'||stringElement=='Q'||stringElement=='s'||
+						stringElement=='S'||stringElement=='t'||stringElement=='T'||stringElement=='v'||stringElement=='V'||
+						stringElement=='x'||stringElement=='X'||stringElement=='z'||stringElement=='Z'||stringElement=='h'||
+						stringElement=='H'||stringElement=='r'||stringElement=='R'||stringElement=='w'||stringElement=='W'||
+						stringElement=='y'||stringElement=='Y')
+				{
+					anyConsonants=true;
+				}
+			}
+			if(anyConsonants==false)
+				throw new ListProcessorException("There are no consonants!");
+
+
+		}
 		Collections.sort(list);
 		Collections.sort(list,(o1, o2) -> o1.replaceAll("[^bBcCdDfFgGjJkKlLmMnNpPqQsStTvVxXzZhHrRwWyY]","").length()-o2.replaceAll("[^bBcCdDfFgGjJkKlLmMnNpPqQsStTvVxXzZhHrRwWyY]","").length());
 		return list;
@@ -136,7 +177,9 @@ public class ListProcessor implements IListProcessor {
 	@Override
 	public List<String> changeByPlacesFirstAndLastSymbolsInEachSecondStringOfList(List<String> list) {
 		if (list==null) throw new ListProcessorException("The list is null");
-		if (list.isEmpty()||list.contains("")) throw new ListProcessorException("The list is empty or contains empty elements");
+		if (list.isEmpty()) throw new ListProcessorException("The list is empty");
+		if (list.size()==1) throw new ListProcessorException("There is only one element");
+
 		List <String> listChanged =new ArrayList<>();
 		for (String listElement: list){
 			if (list.indexOf(listElement)%2==0||listElement.length() <2) listChanged.add(listElement);
@@ -163,7 +206,7 @@ public class ListProcessor implements IListProcessor {
 	@Override
 	public List<String> reverseStringsOfList(List<String> list) {
 		if (list==null) throw new ListProcessorException("The list is null");
-		if (list.isEmpty()||list.contains("")) throw new ListProcessorException("The list is empty or contains empty elements");
+		if (list.isEmpty()) throw new ListProcessorException("The list is empty or contains empty elements");
 		List <String> listReversed =new ArrayList<>();
 		for (String listElement: list){
 			if (listElement.length() < 2) listReversed.add(listElement);
